f01780f2dc145048570d982152c1f55ec459e429
emerson <emerson@emerson-PC>
2013-09-27 00:25:02 -0300

    index

---
+++ simposio/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $local = "Location: presentation/index.php";
+            header($local);
+        ?>
+    </body>
+</html>

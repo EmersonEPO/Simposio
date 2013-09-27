db2814b670ae6d6af5cab52a8b0bf57611623af7
emerson <emerson@emerson-PC>
2013-09-27 00:24:09 -0300

    domainModel

---
+++ simposio/domainModel/Usuario.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usuario
+ *
+ * @author emerson
+ */
+class Usuario {
+    // Objetos
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+    private $status;
+    
+    // Construtor
+    function __construct() {
+        $this->id = 0;
+        $this->nome = "vazio";
+        $this->email = "vazio";
+        $this->senha = "vazio";
+        $this->status = 1;
+    }
+    
+    // Metodos Set e Get
+    //id
+    public function setId($id){
+        $this->id = addslashes($id);
+    }
+    public function getId(){
+        return $this->id;
+    }
+    //nome
+    public function setUsuario($nome){
+        $this->nome = addslashes($nome);
+    }
+    public function getUsuario(){
+        return $this->nome;
+    }
+    //email
+    public function setEmail($email){
+        $this->email = addslashes($email);
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    //senha
+    public function setSenha($senha){
+        $this->senha = addslashes($senha);
+    }
+    public function getSenha(){
+        return $this->senha;
+    }
+    //status
+    public function setStatus($status){
+        $this->status = addslashes($status);
+    }
+    public function getStatus(){
+        return $this->status;
+    }
+}
+
+?>

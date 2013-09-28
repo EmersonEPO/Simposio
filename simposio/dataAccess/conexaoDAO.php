103a6fae6fb5d38b1deaf2639bd60c4921028c47
emerson <emerson@emerson-PC>
2013-09-28 11:38:21 -0300

    criação de classe conexaoDAO

---
+++ simposio/dataAccess/conexaoDAO.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of conexaoDAO
+ *
+ * @author emerson
+ */
+class conexaoDAO {
+    private $user = "root";
+    private $passwd = "epo123";
+    private $server = "localhost";
+    private $database = "bdevento";
+    
+    
+
+    //inicia uma conexao
+    public function conectar(){
+        $conexao = mysql_connect($this->server, $this->user, $this->passwd) or die(mysql_error());
+       
+        return $conexao;        
+    }
+    
+    //selecionar banco
+    public function selecionarBanco(){
+        $base = mysql_select_db($this->database) or die(mysql_errno());
+        
+        if($base){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    //execultar query
+    public function executeQuery($sql){
+        $query = mysql_query($sql) or die(mysql_error());
+        
+        return $query;
+        
+    }
+
+    //fecha a conexao
+    public function desconectar($conexaoAberta){
+        //se a conexao estiver aberta será fechada
+        if ($conexaoAberta){
+            mysql_close($conexaoAberta) or die(mysql_error());
+        }
+    }
+}
+
+?>
